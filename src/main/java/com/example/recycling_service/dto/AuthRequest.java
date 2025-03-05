f34a5a605a8f34a5e31f68743deda625355d7610
pdolinin <paveldolinin03@gmail.com>
2025-03-05 22:30:12 +0300

    first commit создать подключение без авториазации

--- src/main/java/com/example/recycling_service/dto/AuthRequest.java
+++ src/main/java/com/example/recycling_service/dto/AuthRequest.java
@@ -1,24 +1,9 @@
 package com.example.recycling_service.dto;
 
+import lombok.Data;
+
+@Data
 public class AuthRequest {
     private String username;
     private String password;
-
-    // Геттеры и сеттеры
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
